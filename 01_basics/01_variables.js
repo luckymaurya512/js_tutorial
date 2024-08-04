e40149298049be9110351a8fe07801060ccfcbb8
Lucky Maurya <118681697+luckymaurya512@users.noreply.github.com>
2024-08-04 17:59:11 +0000

    variables added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const acc_id= 123
+let email="luckymaurya@google.com"
+var password = 121324
+city="Azamgarh"
+let state
+// const acc_id= 123 constants cannot be changed
+email="lucky@google.com"
+password = 121324574
+city="Roorkee"
+console.log(acc_id)
+console.table([acc_id, email, password, city, state])
