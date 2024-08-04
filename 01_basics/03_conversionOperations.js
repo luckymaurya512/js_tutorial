bfa6daafb94f12e2cdfdfbccb705c292d428e884
Lucky Maurya <118681697+luckymaurya512@users.noreply.github.com>
2024-08-04 18:33:48 +0000

    Conversion of data types

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score=33
+console.log(score)
+console.log(typeof score)
+
+let goal="33"
+console.log(typeof goal)
+
+let points= "33aBC"
+console.log(typeof points)
+
+let temp=Number(points)
+console.log(typeof temp)
+console.log(temp) // its not a number
+
+let a=1   // 1=>true  ,  0=> false
+let b= Boolean(a)   // ""=> false, any other -> true
+console.log(b)
