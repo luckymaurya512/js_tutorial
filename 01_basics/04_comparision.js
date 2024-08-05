03062fa5972f9853ea39401721fdd604b9d3a373
Lucky Maurya <118681697+luckymaurya512@users.noreply.github.com>
2024-08-05 10:33:02 +0000

    strict check comparision

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -1,7 +1,10 @@
-console.log("2">1)
-console.log("2"<1)
-console.log(1>"2")
-console.log("2">"2")
+// console.log("2">1)
+// console.log("2"<1)
+// console.log(1>"2")
+// console.log("2">"2")
+console.log(2=="2")       // chechs only value.. as it internally typecast it into same data type
+
+console.log(2==="2")     // strict check.. check value as well as data type
 
 console.log(null ==0)
 console.log(null >0)
